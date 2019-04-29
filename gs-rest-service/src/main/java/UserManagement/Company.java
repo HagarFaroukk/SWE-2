c1ec7f494597fa2b8943d94922e4cd173468188b
Hagar Farouk <hagar.faroukk@outlook.com>
2019-04-30 01:14:02 +0300

    company classes

--- gs-rest-service/src/main/java/UserManagement/Company.java
+++ gs-rest-service/src/main/java/UserManagement/Company.java
@@ -1,4 +1,50 @@
 package UserManagement;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Company {
+    public String name,username,location,password;
+    public int nOfEmployees;
+    public ArrayList<String> Interests;
+    public Company(String name, String username, String location, String password, int nOfEmployees, ArrayList<String> Interests){
+        this.name = name;
+        this.nOfEmployees = nOfEmployees;
+        this.password = password;
+        this.location = location;
+        this.username = username;
+        this.Interests = Interests;
+    }
+    public Company(String name, String username, String location, int nOfEmployees, ArrayList<String> Interests){
+        this.name = name;
+        this.nOfEmployees = nOfEmployees;
+        this.location = location;
+        this.username = username;
+        this.Interests = Interests;
+    }
+    public Company(String username,ArrayList<String> Interests){
+        this.username = username;
+        this.Interests = Interests;
+    }
+    public Company(){}
+    public Company createAccount(Company c) throws SQLException {
+        CompanyDataAccess cda = new CompanyDataAccess();
+        if(cda.usernameIsAlreadyTaken(c.username)){
+            return null;
+        }
+        cda.insertNewCompany(c);
+        return c;
+    }
+
+    public Company logIn(String username, String password) throws SQLException {
+        CompanyDataAccess cda= new CompanyDataAccess();
+        if(cda.usernameIsAlreadyTaken(username)){
+            if(cda.Check_Password_match(username,password)){
+                return cda.getCompanyByUsername(username);
+            }
+        }
+        return null;
+    }
+
+
 }
--- gs-rest-service/src/main/java/UserManagement/CompanyController.java
+++ gs-rest-service/src/main/java/UserManagement/CompanyController.java
@@ -1,4 +1,16 @@
 package UserManagement;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class CompanyController {
+    public Company signUp(String name, String username, String location, String password, int nOfEmployees, ArrayList<String> Interests) throws SQLException {
+        Company u = new Company(name,username,location,password,nOfEmployees,Interests);
+        return u.createAccount(u);
+    }
+
+    public Company signIn(String username,String password) throws SQLException {
+        Company c = new Company();
+        return c.logIn(username,password);
+    }
 }
